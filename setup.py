2e46b4d09042a2068903578e043cb124dbb31661
Judith Marrero <judimf99@gmail.com>
2022-11-29 14:49:02 +0100

    Made the repository pip installable by adding requirements and setup files. Changed repository files structure.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DSSS-Homework-5',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/jmarrerof/DSSS-Homework-5',
+    license='Apache license 2.0',
+    author='Judith Marrero Ferrera',
+    author_email='judimf99@gmail.com',
+    description='Code to draw colored snowflakes. Made for Homework 5 exercises of the course Data Science Survival Skills, WS 2022.'
+)
