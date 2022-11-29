710760739e145ac900b280951f7ec2151812b7dc
Judith Marrero <judimf99@gmail.com>
2022-11-29 15:34:00 +0100

    Make the repository pip installable by adding requirements and setup files. Change repository structure.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='DSSS-Homework-5',
     version='1.0.0',
-    packages=['snowflake', 'let_it_snow'],
+    packages=['snowflake'],
     url='https://github.com/jmarrerof/DSSS-Homework-5',
     license='Apache license 2.0',
     author='Judith Marrero Ferrera',
